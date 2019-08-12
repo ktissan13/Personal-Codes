649e5b46b044de20e5ee36499e7a842b2d3d959a
Tissan Kugathas <32685188+ktissan13@users.noreply.github.com>
2019-08-12 13:39:29 -0400

    Add files via upload

---
+++ Anime Checker.py
@@ -0,0 +1,11 @@
+# Tissan Kugathas
+# Complete Anime Checker
+# August 12 2019
+
+import os
+
+# ANIME - Shortcut.lnk
+# /Naruto Shippuden/Naruto Shippuden - 1.mp4
+# status = os.ANIME.exists('Naruto Shippuden')
+
+print(os.listdir('ANIME - Shortcut.lnk'))
